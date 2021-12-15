7e6fdd20fe56eef960819d527a3fa0887c053916
AresDan <morozovskiia@gmail.com>
2021-12-15 05:59:02 -0600

    CUDA fix

--- CUDA/example_privatized_bins.cu
+++ CUDA/example_privatized_bins.cu
@@ -51,103 +51,103 @@ __global__ void histoKernel(int *A, int *H, int inputSize) {
 }
 
 int main(void) {
-	int *A_h;
-	int *H_h;
-	int *Valid;
-	int *A_d;
-	int *H_d;
+    int *A_h;
+    int *H_h;
+    int *Valid;
+    int *A_d;
+    int *H_d;
 
-	// Set Device
-	cudaSetDevice(0);
+    // Set Device
+    cudaSetDevice(0);
 
-	// See random number generator
-	srand(time(NULL));
+    // See random number generator
+    srand(time(NULL));
 
-	cout << "Host allocation...\n";
-	A_h = new int[NUM_ELEM];
-	H_h = new int[NUM_BINS];
-	Valid_array = new int[NUM_BINS];
+    cout << "Host allocation...\n";
+    A_h = new int[NUM_ELEM];
+    H_h = new int[NUM_BINS];
+    Valid_array = new int[NUM_BINS];
 
-	cout << "Filling arrays...\n";
+    cout << "Filling arrays...\n";
 
-	default_random_engine generator;
-	normal_distribution<double> distribution(6.0, 2.5);
+    default_random_engine generator;
+    normal_distribution<double> distribution(6.0, 2.5);
 
-	for (int i = 0; i < NUM_ELEM; i++) {
-		double temp = distribution(generator);
-		A_h[i] = (temp < 0.0) ? 0 : ((temp < 10.0) ? ((int)temp) : 9);
-	}
-	for (int i = 0; i < NUM_BINS; ++i)
-		Valid_array[i] = 0;
+    for (int i = 0; i < NUM_ELEM; i++) {
+        double temp = distribution(generator);
+        A_h[i] = (temp < 0.0) ? 0 : ((temp < 10.0) ? ((int)temp) : 9);
+    }
+    for (int i = 0; i < NUM_BINS; ++i)
+        Valid_array[i] = 0;
 
-	cout << "Device allocation...\n";
-	cudaMalloc((void **)&A_d, sizeof(int) * NUM_ELEM);
-	cudaMalloc((void **)&H_d, sizeof(int) * NUM_BINS);
+    cout << "Device allocation...\n";
+    cudaMalloc((void **)&A_d, sizeof(int) * NUM_ELEM);
+    cudaMalloc((void **)&H_d, sizeof(int) * NUM_BINS);
 
-	cout << "Moving arrays to the device...\n";
-	cudaMemcpy(A_d, A_h, sizeof(int) * NUM_ELEM, cudaMemcpyHostToDevice);
+    cout << "Moving arrays to the device...\n";
+    cudaMemcpy(A_d, A_h, sizeof(int) * NUM_ELEM, cudaMemcpyHostToDevice);
 
     // Calculation on host
-	cout << "Calculation on host...\n";
-	for (int i = 0; i < NUM_ELEM; i++) {
-		Valid_array[A_h[i]] += 1;
-	}
+    cout << "Calculation on host...\n";
+    for (int i = 0; i < NUM_ELEM; i++) {
+        Valid_array[A_h[i]] += 1;
+    }
 
     // grid size init
     int grid_dim_x 	= 	ceil(NUM_ELEM / BLOCK_SIZE);
     dim3 gridSize(grid_dim_x, 1, 1);
 
-	for (int i = 0; i < NUM_BINS; ++i) {
-		H_h[i] = 0;
-	}
+    for (int i = 0; i < NUM_BINS; ++i) {
+        H_h[i] = 0;
+    }
 
-	cudaMemcpy(H_d, H_h, sizeof(int) * NUM_BINS, cudaMemcpyHostToDevice);
+    cudaMemcpy(H_d, H_h, sizeof(int) * NUM_BINS, cudaMemcpyHostToDevice);
 
     // Launching kernel
     cout << "Launch kernel...\n";
     histoKernel<<<gridSize, BLOCK_SIZE>>>(A_d, H_d, NUM_ELEM);
 
-	cudaDeviceSynchronize();
+    cudaDeviceSynchronize();
 
-	cout << "Transferring results back to host...\n";
+    cout << "Transferring results back to host...\n";
     cudaMemcpy(H_h, H_d, sizeof(int) * NUM_BINS, cudaMemcpyDeviceToHost);
 
     // Verify results on host
-	cout << "Verify results on host...\n";
+    cout << "Verify results on host...\n";
 
-	bool valid = true;
-	for (int i = 0; i < NUM_BINS; i++) {
-		if (H_h[i] != Valid_array[i]) {
-			Valid_array = false;
-		    break;
-		}
-	}
+    bool valid = true;
+    for (int i = 0; i < NUM_BINS; i++) {
+        if (H_h[i] != Valid_array[i]) {
+            valid = false;
+            break;
+        }
+    }
 
-	if (valid)
-		cout << "GPU results are valid.\n";
-	else
-		cout << "GPU results are invalid.\n";
+    if (valid)
+        cout << "GPU results are valid.\n";
+    else
+        cout << "GPU results are invalid.\n";
 
 
     // Show percentage distribution
-	cout << "Bins distribution: ";
-	for (int i = 0; i < NUM_BINS; ++i) {
-		cout << round(V[i] * 100.0 / NUM_ELEM) << "%, ";
-	}
+    cout << "Bins distribution: ";
+    for (int i = 0; i < NUM_BINS; ++i) {
+        cout << round(V[i] * 100.0 / NUM_ELEM) << "%, ";
+    }
 
     // Memory freeing
-	cout << "Memory freeing on device...\n";
+    cout << "Memory freeing on device...\n";
     cudaFree((void *)A_d);
     cudaFree((void *)H_d);
     cudaDeviceReset();
 
-	cout << "Memory freeing on host...\n";
+    cout << "Memory freeing on host...\n";
 
-	delete[] A_h;
-	delete[] H_h;
-	delete[] Valid_array;
+    delete[] A_h;
+    delete[] H_h;
+    delete[] Valid_array;
 
-	cout << "Program exit.\n";
+    cout << "Program exit.\n";
 
-	return 0;
+    return 0;
 }
